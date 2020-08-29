2e7a27378c279d14b4303e11ea846ea673b911a1
iacopo <iac.pacini@gmail.com>
2020-08-29 17:15:10 +0200

    ultimated SymmetricEncryptionManager

--- Libraries/SymmetricEncryptionManager.cpp
+++ Libraries/SymmetricEncryptionManager.cpp
@@ -4,109 +4,133 @@
 
 #include "SymmetricEncryptionManager.h"
 
-SymmetricEncryptionManager::SymmetricEncryptionManager(unsigned char* enc, unsigned char* iv, unsigned char* hmac) {
-    this->aesManager = new AESManager(enc,iv);
-    this->hmacManager = new HMACManager(hmac);
+SymmetricEncryptionManager::SymmetricEncryptionManager(unsigned char* key, size_t keylen) {
 
+    this->aesKey = new unsigned char[keylen];
+    this->aesKeyLen = keylen;
+    memcpy(this->aesKey,key,this->aesKeyLen);
+
+    RAND_poll();
 }
-SymmetricEncryptionManager::SymmetricEncryptionManager() {
-    this->aesManager = new AESManager();
-    this->hmacManager = new HMACManager();
-}
 
 
-unsigned char *SymmetricEncryptionManager::decryptNVerifyMACThisMessage(unsigned char* toDecryptNVerify, size_t& messageLength){
+unsigned char *
+SymmetricEncryptionManager::encryptThisMessage(unsigned char *plaintext, size_t& plaintext_len, unsigned char *aad,
+                                               size_t aad_len, unsigned char*& iv, size_t& iv_len, unsigned char*& tag) {
 
-    size_t digestLength = EVP_MD_size(this->hmacManager->getHashFunction());
-    if(messageLength <= digestLength){
-        std::cerr<<"FATAL ERROR: received a message whose length is shorter than message digest"<<std::endl;
-        messageLength = 0;
-        return nullptr;
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int ciphertext_len;
+    unsigned char* ciphertext = new unsigned char[plaintext_len + EVP_CIPHER_block_size(this->cipher)];
+    iv_len = AESGCMIVLENGTH ;
+    iv = new unsigned char[iv_len];
+    tag = new unsigned char[AESGCMTAGLENGTH];
+    RAND_bytes(iv,iv_len);
 
+    // Create and initialise the context
+    if(!(ctx = EVP_CIPHER_CTX_new())) {
+        std::cout<<" Error in creating the context for encryption"<<std::endl;
+        goto ENCRYPTIONERROR;
     }
-    std::cout<<"messageLength = "<< messageLength <<std::endl;
-    std::cout<<"digestLength: "<<digestLength<<std::endl;
+    // Initialise the encryption operation.
+    if(1 != EVP_EncryptInit(ctx, EVP_aes_128_gcm(), this->aesKey, iv)) {
+        std::cout<<"Error in Initialising the encryption operation"<<std::endl;
+        goto ENCRYPTIONERROR;
+    }
+    //Provide any AAD data. This can be called zero or more times as required
+    if(1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len)){
+        std::cout<<" Error in providing AAD"<<std::endl;
+        goto ENCRYPTIONERROR;
+    }
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)){
+        std::cout<<"Error in performing encryption"<<std::endl;
+        goto ENCRYPTIONERROR;
+    }
+    ciphertext_len = len;
+    //Finalize Encryption
+    if(1 != EVP_EncryptFinal(ctx, ciphertext + len, &len)){
+        std::cout<<"Error in finalizing encryption"<<std::endl;
+        goto ENCRYPTIONERROR;
+    }
+    ciphertext_len += len;
 
-    // This message is composed by two contiguos chunks: 1 encrypted with AES128 and 1 chunk long exactly 32 byte
-    // that is the HMAC of the encrypted cipherText. The part to be decrypted is the one formed by the firsts
-    // aeskeylength bytes
+    /* Get the tag */
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_GET_TAG, AESGCMTAGLENGTH, tag)){
+        std::cout<<"Error in retrieving the tag "<<std::endl;
+        goto ENCRYPTIONERROR;
+    }
+    /* Clean up */
 
-    size_t HMACLength =  SHA256DIGESTLENGTH;
-    size_t chunkToBeDecryptedLen  = messageLength - SHA256DIGESTLENGTH;
-    std::cout<<"HMACLength = "<< HMACLength <<std::endl;
+    EVP_CIPHER_CTX_free(ctx);
+    plaintext_len = ciphertext_len;
+    return ciphertext;
 
-    if(!this->hmacManager->verifyMAC(&toDecryptNVerify[chunkToBeDecryptedLen], toDecryptNVerify, chunkToBeDecryptedLen)){
-        std::cerr<<"FATAL ERROR: HMAC DOES NOT COINCIDES!!"<<std::endl;
-        messageLength = 0;
-        return nullptr;
-    }else
-        std::cout<<"MAC VERIFIED CORRECTLY"<<std::endl;
-
-    unsigned char* decryptedChunk = this->aesManager->decryptThisMessage(toDecryptNVerify, chunkToBeDecryptedLen);
-    messageLength = chunkToBeDecryptedLen;
-    return decryptedChunk;
-
+    ENCRYPTIONERROR:
+    delete [] ciphertext;
+    delete [] iv;
+    delete [] tag;
+    aad_len = plaintext_len = iv_len = 0;
+    EVP_CIPHER_CTX_cleanup(ctx);
+    return nullptr;
 }
 
-unsigned char *SymmetricEncryptionManager::encryptNMACThisMessage(unsigned char* m, size_t& l) {
-    size_t encryptedMessageLength, hmacLength, totalLength;
+unsigned char *
+SymmetricEncryptionManager::decryptThisMessage(unsigned char *ciphertext, size_t &ciphertext_len, unsigned char *aad,
+                                               size_t aad_len, unsigned char *tag, unsigned char *iv) {
+    unsigned char * plaintext = new unsigned char[ciphertext_len];
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int plaintext_len;
+    int ret;
 
-    unsigned char* encryptedMessage = this->aesManager->encryptThisMessage(m, l);
-
-    if(!encryptedMessage){
-        return encryptedMessage;
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())){
+        std::cout<<" Error in creating the context for decryption"<<std::endl;
+        goto DECRYPTIONERROR;
     }
 
-    encryptedMessageLength = l;
-    totalLength = encryptedMessageLength;
+    if(!EVP_DecryptInit(ctx, EVP_aes_128_gcm(), this->aesKey, iv)){
+        std::cout<<"Error in Initialising the decryption operation"<<std::endl;
+        goto DECRYPTIONERROR;
+    }
+    //Provide any AAD data.
+    if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len)){
+        std::cout<<" Error in providing AAD"<<std::endl;
+        goto DECRYPTIONERROR;
+    }
+    //Provide the message to be decrypted, and obtain the plaintext output.
+    if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)){
+        std::cout<<" Error in providing AAD"<<std::endl;
+        goto DECRYPTIONERROR;
+    }
+    plaintext_len = len;
+    /* Set expected tag value.*/
+    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG, AESGCMTAGLENGTH, tag)){
+        std::cout<<" Error in providing AAD"<<std::endl;
+        goto DECRYPTIONERROR;
+    }
+    /*
+      Finalise the decryption. A positive return value indicates success,
+      anything else is a failure,namely the plaintext is not trustworthy.
+     */
+    if((EVP_DecryptFinal(ctx, plaintext + len, &len)) <=0){
+        std::cout<<" Error Plaintext nt valid"<<std::endl;
+        goto DECRYPTIONERROR;
+    }
 
-    unsigned char* hmacBuffer = this->hmacManager->computeMAC(encryptedMessage, l);
-    if(!hmacBuffer) return hmacBuffer;
-    hmacLength = l;
-    totalLength += hmacLength;
+    plaintext_len += len;
+    ciphertext_len = plaintext_len;
+    EVP_CIPHER_CTX_cleanup(ctx);
+    return plaintext;
 
-    auto* encPlusMacBuffer = new unsigned char[totalLength];
-
-    memcpy(encPlusMacBuffer,encryptedMessage,encryptedMessageLength);
-    memcpy(&encPlusMacBuffer[encryptedMessageLength],hmacBuffer,hmacLength);
-
-    l = totalLength;
-
-
-    return encPlusMacBuffer;
+    DECRYPTIONERROR:
+        EVP_CIPHER_CTX_cleanup(ctx);
+        delete [] plaintext;
+        delete [] tag;
+        return nullptr;
 }
 
-unsigned char *SymmetricEncryptionManager::getAESKey() {
-    return this->aesManager->getAESKey();
+SymmetricEncryptionManager::~SymmetricEncryptionManager() {
+    memset(this->aesKey,0X00,this->aesKeyLen);
+    delete [] this->aesKey;
 }
-
-
-unsigned char *SymmetricEncryptionManager::getHMacKey() {
-    return this->hmacManager->getHMacKey();
-}
-
-unsigned char *SymmetricEncryptionManager::getIV() {
-    return this->aesManager->getIV();
-}
-
-void SymmetricEncryptionManager::setAESKey(unsigned char *k) {
-
-    this->aesManager->setAESKey(k);
-}
-
-void SymmetricEncryptionManager::setAESIV(unsigned char *iv) {
-    this->aesManager->setAESIV(iv);
-}
-
-void SymmetricEncryptionManager::sethmacKey(unsigned char * hk) {
-    this->hmacManager->sethmacKey(hk);
-}
-
-unsigned char *SymmetricEncryptionManager::computeMac(unsigned char * txt, size_t & l) {
-    return this->hmacManager->computeMAC(txt,l);
-}
-
-bool SymmetricEncryptionManager::verifyMac(unsigned char * hmacRec, unsigned char* txtToMac,size_t& txtl) {
-    return this->hmacManager->verifyMAC(hmacRec,txtToMac,txtl);
-}
-
