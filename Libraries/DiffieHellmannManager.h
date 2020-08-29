02e0a41762708760e58fac898a928f84c5ff6664
iacopo <iac.pacini@gmail.com>
2020-08-29 14:42:34 +0200

    added definition of DiffieHellmannManager

--- Libraries/DiffieHellmannManager.h
+++ Libraries/DiffieHellmannManager.h
@@ -5,9 +5,26 @@
 #ifndef ALL_DIFFIEHELLMANNMANAGER_H
 #define ALL_DIFFIEHELLMANNMANAGER_H
 
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<cstring>
+#include<openssl/pem.h>
+#include<openssl/evp.h>
+#include<openssl/rand.h>
+#include<openssl/err.h>
 
 class DiffieHellmannManager {
-
+private:
+    EVP_PKEY* peerPubKey;
+    EVP_PKEY* myPubKey;
+    unsigned char sharedSecret;
+    unsigned char* computeSharedSecret();
+public:
+    DiffieHellmannManager();
+    void setPeerPubKey(unsigned char*,size_t);
+    unsigned char* getMyPubKey(size_t & );
+    unsigned char* getSharedSecret(size_t&);
 };
 
 
