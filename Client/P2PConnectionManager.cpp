e3a6f72d0676fe1630e632fa90e095d17bba38b6
Laura <57455223+llemmi@users.noreply.github.com>
2020-09-02 09:51:16 +0200

    Some changes in UCM. Created P2PConnectionManager class.

---
+++ Client/P2PConnectionManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Laura Lemmi on 02/09/2020.
+//
+
+#include "P2PConnectionManager.h"
+
+P2PConnectionManager::P2PConnectionManager(EVP_PKEY *, ServerConnectionManager *) {
+
+}
+
+P2PConnectionManager::~P2PConnectionManager() {
+
+}
---
+++ Client/P2PConnectionManager.h
@@ -0,0 +1,53 @@
+//
+// Created by Laura Lemmi on 02/09/2020.
+//
+
+#ifndef ALL_P2PCONNECTIONMANAGER_H
+#define ALL_P2PCONNECTIONMANAGER_H
+#include "../Libraries/Constant.h"
+#include "../Libraries/SignatureManager.h"
+#include "../Libraries/SymmetricEncryptionManager.h"
+#include <string>
+#include <vector>
+#include <thread>
+#include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <cstdio>
+
+class ServerConnectionManager;
+
+class P2PConnectionManager {
+private:
+    ServerConnectionManager* serverConnectionManager;
+    struct sockaddr_in opponentAddr;
+    struct sockaddr_in myAddr;
+    int opponentSocket;
+    int mySocket;
+    int port;
+    SignatureManager* signatureManager;
+    SymmetricEncryptionManager* symmetricEncryptionManager;
+
+    bool waitForChallengeRConnection();
+    bool connectToChallengeD();
+    bool establishSecureConnectionWithChallengeR();
+    bool establishSecureConnectionWithChallengeD();
+
+public:
+    P2PConnectionManager(EVP_PKEY*,ServerConnectionManager*);
+
+    void startTheGameAsChallengeR();
+    void startTheGameAsChallengeD();
+    void setOpponentIp(struct in_addr);
+
+    ~P2PConnectionManager();
+
+};
+
+
+#endif //ALL_P2PCONNECTIONMANAGER_H
